1f8b7b8ab850fc52c6240c52a54f3fad6091da6c
swapnil <swapnilsk2019@gmail.com>
2024-04-04 12:35:00 +0530

    added new changes in code and generalized the code

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 import os
 
+import yaml
+
 import aws_cdk as cdk
+
 
 from ecs_cdk.ecs_cdk_stack import EcsCdkStack
 
 
 app = cdk.App()
-EcsCdkStack(app, "EcsCdkStack",
+
+with open('repo.yaml', 'r') as envfile:
+    yaml_environment = yaml.safe_load(envfile)
+
+
+for i in range(len(yaml_environment['clusters'])):
+    clusterr = yaml_environment['clusters'][i]['name']
+    service = yaml_environment['services'][i]['name']
+    VPC = yaml_environment['info'][i]['vpc']
+    VPC_ID = yaml_environment['info'][i]['vpc_id']
+    container= yaml_environment['containers'][i]['name']
+    
+
+
+EcsCdkStack(app,f"EcsCdkStack-{i}",clusterr, service, VPC,VPC_ID,container,
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -15,12 +32,12 @@ EcsCdkStack(app, "EcsCdkStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+   # env=cdk.Environment(account=os.getenv('679104321736'), region=os.getenv('ap-south-1')),
 
     # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+    # want to deploy the stack to. */  
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='679104321736', region='ap-south-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
--- ecs_cdk/ecs_cdk_stack.py
+++ ecs_cdk/ecs_cdk_stack.py
@@ -1,23 +1,22 @@
+from variables import LB
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_elasticloadbalancingv2 as elbv2,
+
 )
+
 from constructs import Construct
 
 
 class EcsCdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str,clusterr, service,VPC,VPC_ID,container,**kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc = ec2.Vpc(
-            self,
-            "Test_VPC",
-            ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
-            nat_gateways=1,
-            max_azs=2,
+        vpc = ec2.Vpc.from_lookup( self, id=f"{VPC}" , vpc_id=VPC_ID, 
+        is_default=False
         )
         # Iterate the private subnets
         selection = vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
@@ -25,13 +24,13 @@ class EcsCdkStack(Stack):
         # for subnet in selection.subnets:
         #   pass
 
-        cluster = ecs.Cluster(self, "testcluster", vpc=vpc)
+        cluster = ecs.Cluster(self, id=f"{clusterr}", vpc=vpc)
 
         # added task defination
         fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef", cpu=256)
         
         # added container to task defination
-        container = fargate_task_definition.add_container("WebContainer",  
+        container = fargate_task_definition.add_container(id=f"container",  
         # Used an image from DockerHub
         image=ecs.ContainerImage.from_registry("atharvab/flask_image"),
         # added port mapping
@@ -39,7 +38,7 @@ class EcsCdkStack(Stack):
         )
 
         # ECS service
-        service = ecs.FargateService( self, "Service", cluster=cluster, task_definition=fargate_task_definition,
+        service = ecs.FargateService( self, id=f"{service}", cluster=cluster, task_definition=fargate_task_definition,
         desired_count=2,)
 
         service.connections.allow_from_any_ipv4(
@@ -47,7 +46,7 @@ class EcsCdkStack(Stack):
         )
 
         lb = elbv2.ApplicationLoadBalancer(
-            self, "LoadBalancer", vpc=vpc, internet_facing=True
+            self,LB, vpc=vpc, internet_facing=True
         )
         listener = lb.add_listener("Listener", port=80)
-        target_group1 = listener.add_targets("ECS1", port=80, targets=[service])
+        target_group1 = listener.add_targets("ECS1", port=80, targets=[service]) 
