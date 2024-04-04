6f8e000f10b402f7480ebe6eedc6ecebeda345c4
swapnil <swapnilsk2019@gmail.com>
2024-04-04 19:21:23 +0530

    added new changes

--- ecs_cdk/ecs_cdk_stack.py
+++ ecs_cdk/ecs_cdk_stack.py
@@ -1,4 +1,12 @@
-from variables import LB
+from variables import (
+    LB,
+    image, 
+    port,
+    #inbound,
+    listener_port,
+    cpu
+)
+ 
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
@@ -12,29 +20,45 @@ from constructs import Construct
 
 class EcsCdkStack(Stack):
 
+    """
+    A class representing a stack for creating EC2 instances and associated resources.
+
+    Attributes:
+    - scope: Construct: The parent of this construct (scope).
+    - construct_id: str: The identifier for the construct.
+    """
+
     def __init__(self, scope: Construct, construct_id: str,clusterr, service,VPC,VPC_ID,container,**kwargs) -> None:
+
+        """
+        Initializes a new EcsCdkStack Cluster.
+
+        Args:
+        - scope (Construct): The parent of this construct (scope).
+        - construct_id (str): The identifier for the construct.
+        - **kwargs: Additional keyword arguments for the Stack constructor.
+        """
         super().__init__(scope, construct_id, **kwargs)
 
+
+        # Creating vpc 
         vpc = ec2.Vpc.from_lookup( self, id=f"{VPC}" , vpc_id=VPC_ID, 
         is_default=False
         )
         # Iterate the private subnets
         selection = vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
 
-        # for subnet in selection.subnets:
-        #   pass
-
         cluster = ecs.Cluster(self, id=f"{clusterr}", vpc=vpc)
 
         # added task defination
-        fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef", cpu=256)
+        fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef", cpu=cpu)
         
         # added container to task defination
         container = fargate_task_definition.add_container(id=f"container",  
         # Used an image from DockerHub
-        image=ecs.ContainerImage.from_registry("atharvab/flask_image"),
+        image=ecs.ContainerImage.from_registry(image),
         # added port mapping
-        port_mappings=[ecs.PortMapping(container_port=1532)],
+        port_mappings=[ecs.PortMapping(port)]
         )
 
         # ECS service
@@ -42,11 +66,11 @@ class EcsCdkStack(Stack):
         desired_count=2,)
 
         service.connections.allow_from_any_ipv4(
-            ec2.Port.tcp(1532), "Allow inbound on port 1532"
+            ec2.Port.tcp(port), "Allow inbound on port 1532"
         )
 
         lb = elbv2.ApplicationLoadBalancer(
             self,LB, vpc=vpc, internet_facing=True
         )
-        listener = lb.add_listener("Listener", port=80)
-        target_group1 = listener.add_targets("ECS1", port=80, targets=[service]) 
+        listener = lb.add_listener("Listener", port=listener_port)
+        target_group1 = listener.add_targets("ECS1", port=listener_port, targets=[service]) 
--- variables.py
+++ variables.py
@@ -1,1 +1,6 @@
 LB="Loadb"
+image="atharvab/flask_image"
+port="1532"
+#inbound=1532
+listener_port="80"
+cpu=56
